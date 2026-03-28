d2a0870d5c38c393a1002ea8f91a4307e0242dfd
outsiderAuk <abdimran012@gmail.com>
2026-03-28 20:34:25 +0600

    listingRoutes corrected

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -2,7 +2,7 @@ const express           = require('express');
 const router            = express.Router();
 const path              = require('path');
 const multer            = require('multer');
-const ListingController = require('../controllers/listingController');
+const ListingController = require('../controllers/listingControllers');
 
 
 const storage = multer.diskStorage({
