c4d76c1aece4acf8f6d7c85d64a7b93388c809a3
outsiderAuk <abdimran012@gmail.com>
2026-04-17 21:22:51 +0600

    view updated

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/user',       (req, res) => res.sendFile(path.join(__dirname, 'views', 
 
 require('./socket')(io);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {  
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
