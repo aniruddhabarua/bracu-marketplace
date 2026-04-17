3ed770c83aea6233190246b42b865bd2564a6737
outsiderAuk <abdimran012@gmail.com>
2026-04-17 19:38:31 +0600

    added feature: Recently Viewed Products

---
+++ routes/recentlyViewedRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const RecentlyViewedController = require('../controllers/recentlyViewedControllers');
+const { authenticate } = require('../middleware/auth');
+
+router.use(authenticate);
+router.get('/', RecentlyViewedController.getRecentlyViewed);
+router.post('/', RecentlyViewedController.trackView);
+module.exports = router;
